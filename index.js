3479a8419011bd95bf0fcaa2c3c6a1a2767dd81d
cerodriguezz <141981427+cerodriguezz@users.noreply.github.com>
2023-10-02 07:55:49 -0700

    did max number function

--- index.js
+++ index.js
@@ -95,6 +95,14 @@ function getMin(numbers) {
 function getMax(numbers) {
   if (numbers.length === 0)
   return undefined;
+}
+let max = numbers[0];
+for (let i = 1; i < numbers.length; i++) {
+  if (numbers[i] > max) {
+    max = numbers [i];
+  }
+  return max;
+}
   // TODO
 }
 
